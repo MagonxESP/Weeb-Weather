5f4d279da3b710f26cf888568fd28bfbc2adbb1b
MagonxESP <janma.360@gmail.com>
2018-02-23 14:16:59 +0100

    code optimizations

--- js/app.js
+++ js/app.js
@@ -1,69 +1,101 @@
-var genericBlackgrounds = [
+var $body;
+var $tiempoContainer;
+var $temperatura;
+var $tempMax;
+var $tempMin;
+var $windSpeed;
+var $ciudad;
+var $sunrise;
+var $sunset;
+var $statusContainer;
+var $statusText;
+var $weatherIcon;
+var $containerWeather;
+
+var genericBackgrounds = [
   'img/1514371209319.jpg',
   'img/1514414573244.jpg',
   'img/1518720443855.jpg'
 ];
 
+var requestUrl = "";
+
+function printData(data) {
+  $ciudad.html(data.name);
+  $temperatura.html(Math.floor(data.main.temp - 273));
+  $tempMax.html(Math.floor(data.main.temp_max - 273));
+  $tempMin.html(Math.floor(data.main.temp_min - 273));
+  $windSpeed.html(data.wind.speed);
+
+  var sunrise = new Date(data.sys.sunrise * 1000);
+  $sunrise.html(sunrise.toLocaleTimeString());
+
+  var sunset = new Date(data.sys.sunset * 1000);
+  $sunset.html(sunset.toLocaleTimeString());
+
+  $body.css('background-image', 'url("img/' + data.weather[0].icon + 'bg.jpg")');
+  $statusContainer.hide();
+  $tiempoContainer.show();
+  $weatherIcon.attr('src', 'img/weather/' + data.weather[0].icon + '_ico.png');
+  $weatherIcon.show();
+}
+
+function getWeather() {
+  $.ajax({
+    url: requestUrl,
+    method: "GET",
+    dataType: "JSON",
+    beforeSend: function() {
+      $statusText.text('Cargando...');
+      $statusContainer.show();
+    },
+    success: function(result){
+      printData(result);
+      updateWeatherTick();
+    },
+    error: function() {
+      $statusText.text('Ciudad no encontrada');
+    }
+  });
+}
+
+function updateWeatherTick() {
+  // actualiza los datos cada 10 minutos
+  setInterval(function(){
+    $.getJSON(requestUrl, function(data) {
+      printData(data);
+      console.log("weather updated!");
+    });
+  }, 600000);
+}
+
 $(document).ready(function() {
-  var $body = $('body');
-  var $tiempoContainer = $('.tiempoContainer');
-  var $temperatura = $('#temperatura');
-  var $tempMax = $('#temperaturaMax');
-  var $tempMin = $('#temperaturaMin');
-  var $windSpeed = $('#windspeed');
-  var $ciudad = $('#ciudad');
-  var $sunrise = $('#sunrise');
-  var $sunset = $('#sunset');
-  var $statusContainer = $('.statusContainer');
-  var $statusText = $('#statusText');
-  var $weatherIcon = $('#weatherImg');
-  var $containerWeather = $('.container-weather');
+  $body = $('body');
+  $tiempoContainer = $('.tiempoContainer');
+  $temperatura = $('#temperatura');
+  $tempMax = $('#temperaturaMax');
+  $tempMin = $('#temperaturaMin');
+  $windSpeed = $('#windspeed');
+  $ciudad = $('#ciudad');
+  $sunrise = $('#sunrise');
+  $sunset = $('#sunset');
+  $statusContainer = $('.statusContainer');
+  $statusText = $('#statusText');
+  $weatherIcon = $('#weatherImg');
+  $containerWeather = $('.container-weather');
 
   // escoge un fondo de forma aleatoria
-  var random = Math.floor(Math.random() * genericBlackgrounds.length);
+  var random = Math.floor(Math.random() * genericBackgrounds.length);
 
-  $body.css('background-image', 'url(' + genericBlackgrounds[random] + ')');
+  $body.css('background-image', 'url(' + genericBackgrounds[random] + ')');
 
   $('#campoCiudad').on("change", function(){
-    var ciudad = $(this).val();
+    var cityName = $(this).val();
+    requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + ",es&appid=1ebe8fb6c5d2654d9ceb6e243540f115";
     $tiempoContainer.hide();
     $weatherIcon.hide();
-
-    $.ajax({
-      url: "https://api.openweathermap.org/data/2.5/weather?q=" + ciudad + ",es&appid=1ebe8fb6c5d2654d9ceb6e243540f115",
-      method: "GET",
-      dataType: "JSON",
-      beforeSend: function() {
-        $statusText.text('Cargando...');
-        $statusContainer.show();
-      },
-      success: function(result){
-        $ciudad.html(ciudad);
-        $temperatura.html(Math.floor(result.main.temp - 273));
-        $tempMax.html(Math.floor(result.main.temp_max - 273));
-        $tempMin.html(Math.floor(result.main.temp_min - 273));
-        $windSpeed.html(result.wind.speed);
-
-        var sunrise = new Date(result.sys.sunrise * 1000);
-        $sunrise.html(sunrise.toLocaleTimeString());
-
-        var sunset = new Date(result.sys.sunset * 1000);
-        $sunset.html(sunset.toLocaleTimeString());
-
-        $body.css('background-image', 'url("img/' + result.weather[0].icon + 'bg.jpg")');
-
-        $statusContainer.hide();
-        $tiempoContainer.show();
-        $weatherIcon.attr('src', 'img/weather/' + result.weather[0].icon + '_ico.png');
-        $weatherIcon.show();
-      },
-      error: function() {
-        $statusText.text('Ciudad no encontrada');
-      }
-    });
-
+    getWeather();
     $(this).val("");
   });
-
 
 });
